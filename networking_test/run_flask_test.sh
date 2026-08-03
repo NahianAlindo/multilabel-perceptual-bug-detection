2519b4065133cd4ff13d8707f81b8529495cf76c
NahianRifaatAlindo <nrifaat26@gmail.com>
2026-08-02 23:19:29 -0400

    ngrok test shell script fix

--- networking_test/run_flask_test.sh
+++ networking_test/run_flask_test.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:h100:1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=8G
-#SBATCH --time=00:15:00
+#SBATCH --time=01:15:00
 #SBATCH --job-name=flask_reach_test
 #SBATCH --output=/home/nahian26/scratch/logs/flask_reach_test_%j.out
 #SBATCH --error=/home/nahian26/scratch/logs/flask_reach_test_%j.err
